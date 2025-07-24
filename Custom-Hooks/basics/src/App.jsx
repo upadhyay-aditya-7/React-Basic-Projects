0d9f0d794fd4c2e72a77b498b8b8c4bdb1536c06
Aditya Upadhyay <adityaupadhyay727@gmail.com>
2025-07-24 18:25:12 +0530

    Added UseIsOnline Hook Component

--- Custom-Hooks/basics/src/App.jsx
+++ Custom-Hooks/basics/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import UseFetch2 from './UseFetch';
 import { usePrev } from './UsePrev';
+import UseIsOnline from './UseIsOnline';
 
 function App() {
 
-  //using useFetch2...
+//using useFetch2...
 
   // const { data, loading, error } = UseFetch2("https://jsonplaceholder.typicode.com/users", 3000);
   // console.log(data);
@@ -26,18 +27,27 @@ function App() {
   // )
 
   //using usePrev...
-    const [count, setCount] = useState(0);
-    const prevValue = usePrev(count);
+    // const [count, setCount] = useState(0);
+    // const prevValue = usePrev(count);
+
+    // return (
+    //   <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    //     <h1>Counter with usePrev Hook</h1>
+    //     <p>Count: {count}</p>
+    //     <p>Previous Count: {prevValue}</p>
+    //     <button onClick={() => setCount(c => c+1)}>Increment</button>
+    //     <button onClick={() => setCount(c => c-1)} style={{ marginLeft: '10px' }}>Decrement</button>
+    //   </div>
+    // )
+
+  // using UseIsOnline
+      const isOnline = UseIsOnline();
 
     return (
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>Counter with usePrev Hook</h1>
-        <p>Count: {count}</p>
-        <p>Previous Count: {prevValue}</p>
-        <button onClick={() => setCount(c => c+1)}>Increment</button>
-        <button onClick={() => setCount(c => c-1)} style={{ marginLeft: '10px' }}>Decrement</button>
+      <div>
+        {isOnline ? "🟢 You are Online" : "🔴 You are Offline"}
       </div>
-    )
+    );
 }
 
 
---
+++ Custom-Hooks/basics/src/UseIsOnline.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+
+const UseIsOnline = () => {
+    const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+    useEffect(() => {
+        const updateOnlineStatus = () => setIsOnline(navigator.onLine);
+
+        window.addEventListener("online", updateOnlineStatus);
+        window.addEventListener("offline", updateOnlineStatus);
+
+        // Clean up the event listeners on component unmount
+    return () => {
+        window.removeEventListener('online', updateOnlineStatus);
+        window.removeEventListener('offline', updateOnlineStatus);
+    }
+    }, []);
+
+    return isOnline;
+}
+
+export default UseIsOnline;
