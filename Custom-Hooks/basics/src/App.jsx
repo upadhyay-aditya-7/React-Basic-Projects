31471c289c60eeefccb9fa66caab2f96a3bf3c5d
Aditya Upadhyay <adityaupadhyay727@gmail.com>
2025-07-24 09:17:54 +0530

    Adding UsePrev Hook Component

--- Custom-Hooks/basics/src/App.jsx
+++ Custom-Hooks/basics/src/App.jsx
@@ -1,25 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 import UseFetch2 from './UseFetch';
+import { usePrev } from './UsePrev';
 
 function App() {
 
-  const { data, loading, error } = UseFetch2("https://jsonplaceholder.typicode.com/users", 3000);
-  console.log(data);
+  //using useFetch2...
 
-  if(loading) {
-    return <div>Loading...</div>
-  }
+  // const { data, loading, error } = UseFetch2("https://jsonplaceholder.typicode.com/users", 3000);
+  // console.log(data);
+
+  // if(loading) {
+  //   return <div>Loading...</div>
+  // }
   
 
-  return (
-      <div>
-        <ul>
-        {data.map((d) => (
-          <li key={d.id}>{d.name}</li>
-        ))}
-        </ul>
+  // return (
+  //     <div>
+  //       <ul>
+  //       {data.map((d) => (
+  //         <li key={d.id}>{d.name}</li>
+  //       ))}
+  //       </ul>
+  //     </div>
+  // )
+
+  //using usePrev...
+    const [count, setCount] = useState(0);
+    const prevValue = usePrev(count);
+
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Counter with usePrev Hook</h1>
+        <p>Count: {count}</p>
+        <p>Previous Count: {prevValue}</p>
+        <button onClick={() => setCount(c => c+1)}>Increment</button>
+        <button onClick={() => setCount(c => c-1)} style={{ marginLeft: '10px' }}>Decrement</button>
       </div>
-  )
+    )
 }
 
 
